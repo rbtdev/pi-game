b2ced13263d9191fa5ea3894172259048ab816fa
Rob Thuleen <rob.thuleen@gmail.com>
2016-01-26 10:47:44 -0800

    Refactor

--- game.py
+++ game.py
@@ -1,26 +1,6 @@
-from gpiozero import Button, LED
+import paddle
 from time import sleep
-from datetime import datetime
 import threading
-class Paddle:
-	def __init__(self, owner, buttonPin, ledPin):
-		self.owner = owner
-		self.button = Button(buttonPin)
-		self.led = LED(ledPin)
-		self.led.off()
-		self.pressedAt = None
-
-	def enable(self):
-		self.button.when_pressed = self.handleButtonPress
-	
-	def disable(self):
-		self.button.when_pressed = None
-	
-	def handleButtonPress(self):
-		self.pressedAt = datetime.now()
-		print(self.pressedAt)
-		buttonPressed(self)
-
 	
 class Player:
 	def __init__(self, name, buttonPin, ledPin):
---
+++ paddle.py
@@ -0,0 +1,21 @@
+from gpiozero import Button, LED
+from datetime import datetime
+
+class Paddle:
+	def __init__(self, owner, buttonPin, ledPin):
+		self.owner = owner
+		self.button = Button(buttonPin)
+		self.led = LED(ledPin)
+		self.led.off()
+		self.pressedAt = None
+
+	def enable(self):
+		self.button.when_pressed = self.handleButtonPress
+	
+	def disable(self):
+		self.button.when_pressed = None
+	
+	def handleButtonPress(self):
+		self.pressedAt = datetime.now()
+		print(self.pressedAt)
+		buttonPressed(self)
