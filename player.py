9223021ce5208d5217d90b9b8189d1fa3da84bc1
Rob Thuleen <rob.thuleen@gmail.com>
2016-01-26 10:53:46 -0800

    Refactor

---
+++ player.py
@@ -0,0 +1,19 @@
+class Player:
+	def __init__(self, name, buttonPin, ledPin):
+		self.name = name
+		self.score = 0
+		self.paddle = Paddle(self, buttonPin, ledPin)
+	def enable(self):
+		self.paddle.enable()
+
+	def disable(self):
+		self.paddle.disable()
+
+	def on(self):
+		self.paddle.led.on()
+
+	def off(self):
+		self.paddle.led.off()
+
+	def pressed(self):
+		return self.paddle.button.is_pressed
